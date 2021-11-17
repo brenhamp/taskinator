d36cb84ec62facf9f1e17d81872a3d542624a06a
brenhamp <bpozzi@trinity.edu>
2021-11-16 18:48:46 -0600

    create new tasks that can be labeled

---
+++ .history/assets/js/script_20211116184616.js
@@ -0,0 +1,16 @@
+var formEl = document.querySelector("#task-form"); 
+var tasksToDoEl = document.querySelector("#tasks-to-do"); 
+
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  console.dir(taskNameInput);
+
+  var listItemEl = document.createElement("li"); 
+  listItemEl.className = "task-item"; 
+  listItemEl.textContent = taskNameInput;
+  tasksToDoEl.appendChild(listItemEl); 
+  }; 
+
+  formEl.addEventListener("submit", createTaskHandler);
